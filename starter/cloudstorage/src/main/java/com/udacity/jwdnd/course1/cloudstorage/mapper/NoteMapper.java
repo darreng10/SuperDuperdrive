6ba70cc87a4717e5584b6238cc05310f95d6392f
darreng10 <darreng.10@gmail.com>
2021-02-24 14:14:17 +0000

    add get Note method to NoteMapper

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
@@ -17,6 +17,9 @@ public interface NoteMapper {
     @Select("SELECT * FROM NOTES WHERE userid = #{userId}")
     List<Note> getAllNoteByUserId(Integer userId);
 
+    @Select("SELECT * FROM NOTES WHERE notetitle = #{notetitle} AND notedescription = #{notedescription} AND userid = #{userId}")
+    Note getNote(String notetitle, String notedescription, Integer userId);
+
     @Insert("INSERT INTO NOTES (notetitle, notedescription, userid) VALUES(#{notetitle}, #{notedescription}, #{userid})")
     @Options(useGeneratedKeys = true, keyProperty = "noteid")
     int insert (Note note);
